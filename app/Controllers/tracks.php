87bffb7583bcf4f60cdbeb7622d7b9ca2d90428c
Rahman <rahmanromli@yahoo.com>
2016-09-11 00:53:41 +0800

    define model

--- app/Controllers/tracks.php
+++ app/Controllers/tracks.php
@@ -1,10 +1,10 @@
 <?php
-    
+    // use Models\Track;
+
     $tracks = $app['controllers_factory'];
     
-    $tracks->get('/', function() use ($app)
-    {
-        $tracklist = Track::getTrackList();
+    $tracks->get('/', function() use ($app){
+        $tracklist = \App\Models\Track::getTrackList();
         $output = array("data" => $tracklist);
         return json_encode($output);
     });
