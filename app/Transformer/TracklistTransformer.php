a6c9226f5bf7a0d436fbad14148cedddb47a7236
Rahman <rahmanromli@yahoo.com>
2016-09-10 23:08:39 +0800

    add transformer

---
+++ app/Transformer/TracklistTransformer.php
@@ -0,0 +1,14 @@
+<?php
+    namespace Transformer;
+    use Models\Track;
+    
+    class TracklistTransformer extends \League\Fractal\TransformerAbstract{
+        public function transform(Track $track)
+        {
+            return [
+                'id' => (int) $track->id,
+                'title' => $track->title,
+                'artist' => $track->artist_name
+            ];
+        }
+    }
