73e6c1f7799f2598f54050d92942aba808e4ad89
Rahman <rahmanromli@yahoo.com>
2016-09-14 00:05:17 +0800

    add item route for book

--- app/Controllers/book.php
+++ app/Controllers/book.php
@@ -2,12 +2,20 @@
 use App\Models\Book;
 use App\Transformer\BookTransformer;
 use League\Fractal\Resource\Collection;
+use \League\Fractal\Resource\Item;
 
 $cont = $app['controllers_factory'];
 $cont->get('/', function() use ($app){
 	$book = Book::all();
 	$books = new Collection($book, new BookTransformer);
 	$output = $app['serializer']->createData($books)->toArray();
+	return json_encode($output);
+});
+
+$cont->get('/{id}', function($id) use ($app){
+	$book = Book::find($id);
+	$book_a = new Item($book, new BookTransformer);
+	$output = $app['serializer']->createData($book_a)->toArray();
 	return json_encode($output);
 });
 
