324b051638927d101cbcfdbfc8d41b9c6788fffc
Rahman <rahmanromli@yahoo.com>
2016-09-14 03:08:57 +0800

    update borrow route

--- app/Controllers/borrow.php
+++ app/Controllers/borrow.php
@@ -1,10 +1,15 @@
 <?php
 use \App\Models\Borrow;
+use \App\Transformer\BorrowTransformer;
+use \League\Fractal\Resource\Collection;
 
 $contr = $app['controllers_factory'];
 
 $contr->get('/', function()use ($app){
-	return 'ok';
+	$borrow = Borrow::all();
+	$borrows = new Collection($borrow, new BorrowTransformer);
+	$output = $app['serializer']->createData($borrows)->toArray();
+	return json_encode($output);
 });
 
 return $contr;
