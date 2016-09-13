ed076893676fb74d38b97754acb86e40dec4ad4d
Rahman <rahmanromli@yahoo.com>
2016-09-14 03:08:26 +0800

    add borrow transformer

---
+++ app/Transformer/BorrowTransformer.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Transformer;
+use \App\Models\Borrow;
+
+class BorrowTransformer extends \League\Fractal\TransformerAbstract{
+	public function transform(Borrow $borrow){
+		return [
+			'borrow_id'=>$borrow->id,
+			'borrow_book'=>$borrow->book_id,
+			'borrow_user'=>$borrow->user_id,
+			'borrow_updated_at'=>$borrow->updated_at,
+			'borrow_created_at'=>$borrow->created_at
+		];
+	}
+}
