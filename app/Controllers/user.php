abf4d2344ede5faf2c46bf74f0cc04ee67fac579
Rahman <rahmanromli@yahoo.com>
2016-09-14 18:45:06 +0800

    get all user update

--- app/Controllers/user.php
+++ app/Controllers/user.php
@@ -1,9 +1,16 @@
 <?php
+use App\Models\User;
+use \App\Transformer\UserTransformer;
+use \League\Fractal\Resource\Collection;
 
 $con = $app['controllers_factory'];
 
-$con->get('', function()use ($app){
-	return 'ok';
+$con->get('/', function()use ($app){
+	$user = User::all();
+	$users = new Collection($user, new UserTransformer);
+	$app['serializer']->parseIncludes('book');
+	$output = $app['serializer']->createData($users)->toArray();
+	return json_encode($output);
 });
 
 return $con;
--- app/Transformer/UserTransformer.php
+++ app/Transformer/UserTransformer.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Transformer;
 use \App\Models\User;
+use \App\Transformer\BookTransformer;
 
 class UserTransformer extends \League\Fractal\TransformerAbstract{
+	protected $availableIncludes = ['book'];
+
 	public function transform(User $user){
 		return [
 			'user_id'=>(int) $user->id,
@@ -13,5 +16,11 @@ class UserTransformer extends \League\Fractal\TransformerAbstract{
 			'user_name'=>$user->name,
 			'user_address'=>$user->address
 		];
+	}
+
+	public function includeBook(User $user){
+		$book = $user->Book;
+		if($book)
+			return $this->collection($book, new BookTransformer);
 	}
 }
