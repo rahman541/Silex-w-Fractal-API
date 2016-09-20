f188ecb99d30d779a8570b171061067697aafe27
Rahman <rahmanromli@yahoo.com>
2016-09-21 00:26:18 +0800

    update POST /login/

--- app/Controllers/login.php
+++ app/Controllers/login.php
@@ -2,12 +2,23 @@
 use \Firebase\JWT\JWT;
 use \Symfony\Component\HttpFoundation\Request;
 use \Symfony\Component\HttpFoundation\Response;
+use \App\Models\User;
 
 $cont = $app['controllers_factory'];
 
 $cont->post('/', function(Request $request) use ($app){
 	if($request->get('email') && $request->get('password')){
-
+		$email = $request->get('email');
+		$password = $request->get('password');
+		$user = User::where(['email'=>$email,'password'=>$password])->get();
+		if(count($user)==1){
+			return new Response(json_encode(['user'=>$user->first()]),202,['Content-Type'=>'application/json']);
+		}else{
+			return new Response(json_encode([
+				'error'=>true,
+				'message'=>'Login credential invalid!'
+			]), 200, ['Content-Type'=>'application/json']);
+		}
 	}else{
 		return new Response(json_encode([
 			'error'=>true,
