dc531135d35a3270ce01be68e89fa55ea9093346
Rahman <rahmanromli@yahoo.com>
2016-09-21 03:44:21 +0800

    add middleware to require jwt token for route: GET /user & GET /user/{id}

--- app/Controllers/user.php
+++ app/Controllers/user.php
@@ -7,27 +7,29 @@ use Symfony\Component\HttpFoundation\Response;
 
 $con = $app['controllers_factory'];
 
+// GET /user
 $con->get('/', function()use ($app){
 	$user = User::all();
 	$users = new Collection($user, new UserTransformer);
 	$app['serializer']->parseIncludes('book');
 	$output = $app['serializer']->createData($users)->toArray();
 	return new Response(json_encode($output),202, ['Content-Type'=>'application/json']);
-});
+})->before($auth);
 
+// GET /user/{id}
 $con->get('/{id}', function($id) use ($app){
 	$user = User::find($id);
 	if($user){
 		$users = new Item($user, new UserTransformer);
 		$app['serializer']->parseIncludes('book');
 		$output = $app['serializer']->createData($users)->toArray();
-		return json_encode($output);
+		return new Response(json_encode($output),202, ['Content-Type'=>'application/json']);
 	}else{
 		return json_encode([
 			'error'=>false,
 			'message'=>'User not found'
 		]);
 	}
-});
+})->before($auth);
 
 return $con;
--- app/index.php
+++ app/index.php
@@ -1,11 +1,15 @@
 <?php
     require("../vendor/autoload.php");
+    use Firebase\JWT\JWT;
+    use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+    use Silex\Application;
 
     // Configure dot env
     $dotenv = new Dotenv\Dotenv("../");
 	$dotenv->overload();
     
-    $app = new Silex\Application();
+    $app = new Application();
     $app['debug'] = true;
     
     $app['database'] = require("database.php");
@@ -27,6 +31,35 @@
 	$app->get('/', function() use($app) {
 		return $app['twig']->render('home.twig');
 	});
+
+	// Middleware to Veify jwt token auth here
+	$auth = function (Request $request, Application $app) {
+	    if($request->headers->get('authorization')){
+	    	$auth = $request->headers->get('authorization');
+	    	list($jwt) = sscanf( $auth, 'Bearer %s');
+	    	if($jwt){
+	    		try{
+		    		JWT::decode($jwt, getenv('APP_KEY'), array('HS256'));
+	    		}catch(\Exception $e){
+	    			return new Response(json_encode([
+		    			'error'=>true,
+		    			'message'=>'invalid api key',
+					]), 200, ['Content-Type'=>'application/json']);
+	    		}
+
+	    	}else{
+	    		return new Response(json_encode([
+	    			'error'=>true,
+	    			'message'=>'invalid request',
+    			]), 200, ['Content-Type'=>'application/json']);
+	    	}
+	    }else{
+	    	return new Response(json_encode([
+	    			'error'=>true,
+	    			'message'=>'authorization not send',
+			]), 200, ['Content-Type'=>'application/json']);
+	    }
+	};
     
     $app->mount('/tracks', include 'controllers/tracks.php');
 
