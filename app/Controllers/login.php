33e5b2953fac2dbfd08b5215ad802a169ebbe908
Rahman <rahmanromli@yahoo.com>
2016-09-20 23:53:13 +0800

    update POST /login

--- app/Controllers/login.php
+++ app/Controllers/login.php
@@ -1,10 +1,19 @@
 <?php
 use \Firebase\JWT\JWT;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
 
 $cont = $app['controllers_factory'];
 
-$cont->get('/', function(){
-	return 'ok';
+$cont->post('/', function(Request $request) use ($app){
+	if($request->get('email') && $request->get('password')){
+
+	}else{
+		return new Response(json_encode([
+			'error'=>true,
+			'message'=>'Email & Password invalid!'
+		]), 200, ['Content-Type'=>'application/json']);
+	}
 });
 
 return $cont;
